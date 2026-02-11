d69938bcdb9afc9464bf750b7b4339c8b51ed32b
sujeeth <sujeeth.bhandary@codecraft.co.in>
2026-02-11 14:43:06 +0530

    first commit

---
+++ maths.ts
@@ -0,0 +1,28 @@
+
+// Math module implements some basic function
+
+/**
+ * add a and b
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+
+function add(a:number,b:number):number{
+    return a+b
+}
+
+
+function subtract(a:number,b:number):number{
+    return a-b;
+}
+
+/**
+ * if a and b are equal, return true, otherwise return false
+ * @param a add a number a
+ * @param b add a number b
+ * @returns 
+ */
+function boolean(a:number,b:number):boolean{
+    return a===b;
+}
